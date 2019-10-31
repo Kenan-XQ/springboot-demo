281728b2421eccef158dc00cfc5c9528c58aab29
xq <xq@qq.com>
2019-10-31 13:59:21 +0800

    springboot初始化项目

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("/index")
+    @ResponseBody
+    public String index() {
+        return "Hello world";
+    }
+}
