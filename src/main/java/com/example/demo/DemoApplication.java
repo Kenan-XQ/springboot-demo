86dd53a15f9b8fdb91b96567dd366d6d6bbdd057
xq <xq@qq.com>
2020-02-14 10:26:54 +0800

    第三次测试

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,7 @@ public class DemoApplication {
     public static void main(String[] args) {
         System.out.println("第一次测试");
         System.out.println("第二次测试");
+        System.out.println("第三次测试");
         SpringApplication.run(DemoApplication.class, args);
     }
 
